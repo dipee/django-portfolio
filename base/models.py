738ae62fa123e8c1d5ccc8dc1795a5d88353a3c2
dipee <dipen.2052@gmail.com>
2024-01-01 18:58:37 -0500

    profile model added

--- base/models.py
+++ base/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 
+class Profile(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    resume = models.FileField(upload_to='files/resume/')
+    image = models.ImageField(upload_to='images/profile/')
 
+    
 class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
